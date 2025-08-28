9b315ab2dc8e8e9e6999e8d3cc38ad29cac73dfe
Shreeram Narkhede <126403314+shreeram2302@users.noreply.github.com>
2025-08-28 22:18:46 +0530

    Db changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    
-    dialect: process.env.DB_DIALECT,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+});
 
 module.exports = sequelize;
